87d15111b71b1fffbb6f06202a63cd10f5a1bc3f
mikelim <mikelim@mikelimui-MacBookAir.local>
2022-12-04 22:29:31 +0900

    add: 1. array util 2. comment in optional.go

--- optional/optional.go
+++ optional/optional.go
@@ -6,16 +6,19 @@ type Optional[T any] struct {
 	val T
 }
 
+//New - get new Optional
 func New[T any](v T) *Optional[T] {
 	return &Optional[T]{
 		val: v,
 	}
 }
 
+//Get - get value
 func (o *Optional[T]) Get() T {
 	return o.val
 }
 
+//OrElse - get default value
 func (o *Optional[T]) OrElse(n T) T {
 	if reflect.ValueOf(&o.val).Elem().IsZero() {
 		return n
@@ -23,6 +26,7 @@ func (o *Optional[T]) OrElse(n T) T {
 	return o.val
 }
 
+//OrElseError - get value or error
 func (o *Optional[T]) OrElseError(e error) (T, error) {
 	if reflect.ValueOf(&o.val).Elem().IsZero() {
 		return o.val, e
